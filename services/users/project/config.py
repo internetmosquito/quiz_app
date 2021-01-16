60ecc33a3149aa2c82220c49ca3cb1a42c1fe4b4
internetmosquito <alejandrovillamarin@gmail.com>
2021-01-16 18:05:46 +0100

    Initial commit

---
+++ services/users/project/config.py
@@ -0,0 +1,25 @@
+# services/users/project/config.py
+import os
+
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+    # This saves resources, more info at https://stackoverflow.com/questions/33738467/how-do-i-know-if-i-can-disable-sqlalchemy-track-modifications/33790196#33790196
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
